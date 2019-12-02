ac00b983c6f1774332184fcc7c8c45e6ef29eae0
sidra-asa <sasakikung@gmail.com>
2019-12-02 18:03:27 +0800

    Add test_get_method_bytecode in test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,18 +4,26 @@ from androguard.core.mutf8 import MUTF8String
 from androguard.core.analysis.analysis import MethodAnalysis
 
 from quark.main import XRule
+from quark.Objects.BytecodeObject import BytecodeObject
 
 
 @pytest.fixture()
-def xrule_obj(scope="fuction"):
+def xrule_obj1(scope="fuction"):
     apk_file = "quark/sample/13667fe3b0ad496a0cd157f34b7e0c991d72a4db.apk"
+    xrule_obj = XRule(apk_file)
+    yield xrule_obj
+
+
+@pytest.fixture()
+def xrule_obj2(scope="fuction"):
+    apk_file = "quark/sample/14d9f1a92dd984d6040cc41ed06e273e.apk"
     xrule_obj = XRule(apk_file)
     yield xrule_obj
 
 
 class TestXRule():
 
-    def test_permissions(self, xrule_obj):
+    def test_permissions(self, xrule_obj1):
         ans = [
             'android.permission.SEND_SMS',
             'android.permission.RECEIVE_BOOT_COMPLETED',
@@ -26,16 +34,16 @@ class TestXRule():
             'android.permission.RECEIVE_SMS',
             'android.permission.READ_CONTACTS'
         ]
-        assert set(xrule_obj.permissions) == set(ans)
+        assert set(xrule_obj1.permissions) == set(ans)
 
-    def test_find_method(self, xrule_obj):
-        result = list(xrule_obj.find_method("Ljava/lang/reflect/Field"))
+    def test_find_method(self, xrule_obj1):
+        result = list(xrule_obj1.find_method("Ljava/lang/reflect/Field"))
         assert len(result) == 2
         assert isinstance(result[0], MethodAnalysis)
 
-    def test_upperFunc(self, xrule_obj):
-        result = xrule_obj.upperFunc("Landroid/content/ContentResolver",
-                                     "query")
+    def test_upperFunc(self, xrule_obj1):
+        result = xrule_obj1.upperFunc("Landroid/content/ContentResolver",
+                                      "query")
 
         expect_cls = "Lcom/example/google/service/ContactsHelper;"
         expect_func = "getSIMContacts"
@@ -46,3 +54,18 @@ class TestXRule():
         # (Lcom/example/google/service/ContactsHelper;, getSIMContacts)
 
         assert expect_tuple in result
+
+    def test_get_method_bytecode(self, xrule_obj1, xrule_obj2):
+        target1_cls = "Lcom/example/google/service/ContactsHelper"
+        target1_func = "query"
+        result1 = xrule_obj1.get_method_bytecode(target1_cls, target1_func)
+        assert len(list(result1)) == 0
+
+        target2_cls = "Lcom/google/progress/AndroidClientService"
+        target2_func = "sendMessage"
+        result2 = xrule_obj2.get_method_bytecode(target2_cls, target2_func)
+        result2_list = list(result2)
+        assert len(result2_list) == 87
+        result2_mne_para = [(i.mnemonic, i.parameter) for i in result2_list]
+        expect_item = ("new-instance", "Lcom/google/progress/SMSHelper;")
+        assert expect_item in result2_mne_para
